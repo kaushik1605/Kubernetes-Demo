a82c0c7dd394916c030ebbd7e0a4c21389c8de2b
durgadeviramadoss <durgaoct1989@gmail.com>
2018-04-18 12:38:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World-nodejsapp2!')
+  response.send('Welcome to Kubernetes!')
 })
 
 app.listen(app.get('port'), function() {
